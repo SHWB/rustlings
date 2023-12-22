a81a14fbff0d21423a0d96d2048ab51e09015bf4
shwb <angelobarboni.eng@gmail.com>
2023-12-22 01:15:09 +0100

    exercises up to modules included

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,12 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_no: i32) -> i32 {
+    let price_per_apple = if apple_no > 40 { 1 } else { 2 };
+    apple_no * price_per_apple
+}
 
 // Don't modify this function!
 #[test]
